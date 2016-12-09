7755517b4fdda874af656edb56ed915691ab6988
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-12-09 13:50:23 -0800

    Consider device positions while sending messages through a channel

--- netsim/__init__.py
+++ netsim/__init__.py
@@ -3,10 +3,13 @@
 
 from collections import defaultdict, namedtuple, Hashable
 from abc import ABCMeta, abstractmethod
-from typing import List, Tuple, Dict
+from typing import Any, List, Tuple, Dict, DefaultDict
 
 
+DevicePosition = namedtuple('DevicePosition', ['x', 'y'])
+origin = DevicePosition(0, 0)
 Message = object
+DeviceMessagesDict = Dict[str, List[Message]]
 
 
 class Identifiable(Hashable):
@@ -22,15 +25,15 @@ class Identifiable(Hashable):
 
 class AbstractChannel(Identifiable, metaclass=ABCMeta):
     @abstractmethod
-    def step(self, timestamp: int, messages_inserted: List[Message]) \
-            -> List[Message]:
+    def step(self, timestamp: int, messages_inserted: List[Message],
+             device_locations: Dict[str, DevicePosition]) -> DeviceMessagesDict:
         pass
 
 
 class BaseChannel(AbstractChannel):
-    def step(self, timestamp: int, messages_inserted: List[Message]) \
-            -> List[Message]:
-        return messages_inserted
+    def step(self, timestamp: int, messages_inserted: List[Message],
+             device_locations: Dict[str, DevicePosition]) -> DeviceMessagesDict:
+        return {device_id: messages_inserted for device_id in device_locations}
 
 
 class ImmediateIdentityChannel(BaseChannel):
@@ -40,18 +43,11 @@ class ImmediateIdentityChannel(BaseChannel):
 ChannelMessagesDict = Dict[str, List[Message]]
 
 
-def channel_messages_dict_merge_update(
-    d: ChannelMessagesDict,
-    other: ChannelMessagesDict
-) -> ChannelMessagesDict:
+def dict_merge_update(d: DefaultDict[Any, list], other: Dict[Any, list]) \
+        -> DefaultDict[Any, list]:
     for channel_id, messages in other.items():
         d[channel_id].extend(messages)
-
     return d
-
-
-DevicePosition = namedtuple('DevicePosition', ['x', 'y'])
-origin = DevicePosition(0, 0)
 
 
 class AbstractDevice(Identifiable, metaclass=ABCMeta):
@@ -90,16 +86,21 @@ class Simulator(object):
 
         self.next_timestamp = 0
         self.messages_to_insert = defaultdict(list)
+        self.device_positions = {}
 
     def step(self):
-        messages_received = []
+        messages_received = defaultdict(list)
         for channel in self.channels:
-            messages_received.extend(channel.step(
-                self.next_timestamp, self.messages_to_insert[channel.id]))
+            device_messages = channel.step(
+                self.next_timestamp, self.messages_to_insert[channel.id],
+                self.device_positions)
+            dict_merge_update(messages_received, device_messages)
 
         self.messages_to_insert = defaultdict(list)
         for device in self.devices:
-            channel_messages_dict, _ = \
-                device.step(self.next_timestamp, messages_received)
+            channel_messages, position = \
+                device.step(self.next_timestamp, messages_received[device.id])
+            dict_merge_update(self.messages_to_insert, channel_messages)
+            self.device_positions[device.id] = position
 
         self.next_timestamp += self.step_size
