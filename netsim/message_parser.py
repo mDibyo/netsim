9ed08410b7eee4b93348a1ab829e27a7fe45ed0a
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-12-09 16:39:35 -0800

    python3 and pep8'ify message_parser

--- netsim/message_parser.py
+++ netsim/message_parser.py
@@ -1,52 +1,58 @@
-#code adapted to Python from http://stackoverflow.com/questions/19723641/find-intersecting-point-of-three-circles-programmatically
+#!/usr/bin/env python3
+#
+# code adapted to Python from
+# http://stackoverflow.com/questions/19723641/find-intersecting-point-of-three-circles-programmatically
+
+from math import sqrt
+
 
 EPSILON = 0.000001
-import math
+triangulate_output_format = 'INTERSECTION Circle1 AND Circle2 AND Circle3: ({}, {})'
+
 
 def triangulate(x0, y0, r0, x1, y1, r1, x2, y2, r2):
+    # dx and dy are the vertical and horizontal distances between the circle centers.
+    dx = x1 - x0
+    dy = y1 - y0
 
-	# dx and dy are the vertical and horizontal distances between the circle centers.
-	dx = x1 - x0
-	dy = y1 - y0
+    # Determine the straight-line distance between the centers.
+    d = sqrt((dy*dy) + (dx*dx))
 
-	# Determine the straight-line distance between the centers.
-	d = math.sqrt((dy*dy) + (dx*dx))
+    # Check for solvability.
+    if d > (r0 + r1):
+        return False
 
-	# Check for solvability.
-	if (d > (r0 + r1)):
-		return false
+    if d < abs(r0 - r1):
+        return False
 
-	if (d < abs(r0 - r1)):
-		return false
+    a = ((r0*r0) - (r1*r1) + (d*d)) / (2.0 * d)
 
-	a = ((r0*r0) - (r1*r1) + (d*d)) / (2.0 * d)
+    point2_x = x0 + (dx * a/d)
+    point2_y = y0 + (dy * a/d)
 
-	point2_x = x0 + (dx * a/d)
-	point2_y = y0 + (dy * a/d)
+    h = sqrt((r0*r0) - (a*a))
+    rx = -dy * (h/d)
+    ry = dx * (h/d)
 
-	h = math.sqrt((r0*r0) - (a*a))
-	rx = -dy * (h/d)
-	ry = dx * (h/d)
+    intersection_point1_x = point2_x + rx
+    intersection_point2_x = point2_x - rx
+    intersection_point1_y = point2_y + ry
+    intersection_point2_y = point2_y - ry
 
-	intersectionPoint1_x = point2_x + rx
-	intersectionPoint2_x = point2_x - rx
-	intersectionPoint1_y = point2_y + ry
-	intersectionPoint2_y = point2_y - ry
+    dx = intersection_point1_x - x2
+    dy = intersection_point1_y - y2
+    d1 = sqrt((dy*dy) + (dx*dx))
 
-	dx = intersectionPoint1_x - x2
-	dy = intersectionPoint1_y - y2
-	d1 = math.sqrt((dy*dy) + (dx*dx))
+    dx = intersection_point2_x - x2
+    dy = intersection_point2_y - y2
+    d2 = sqrt((dy*dy) + (dx*dx))
 
-	dx = intersectionPoint2_x - x2
-	dy = intersectionPoint2_y - y2
-	d2 = math.sqrt((dy*dy) + (dx*dx))
-
-	if(abs(d1 - r2) < EPSILON):
-		print "INTERSECTION Circle1 AND Circle2 AND Circle3: (" + str(intersectionPoint1_x) + "," + str(intersectionPoint1_y) + ")"
-	elif(abs(d2 - r2) < EPSILON):
-		print "INTERSECTION Circle1 AND Circle2 AND Circle3: (" + str(intersectionPoint2_x) + "," + str(intersectionPoint2_y) + ")"
-	else:
-		print "INTERSECTION Circle1 AND Circle2 AND Circle3: NONE"
+    if abs(d1 - r2) < EPSILON:
+        print(triangulate_output_format.format(intersection_point1_x, intersection_point1_y))
+    elif abs(d2 - r2) < EPSILON:
+        print(triangulate_output_format.format(intersection_point2_x, intersection_point2_y))
+    else:
+        print(triangulate_output_format.format(None, None))
 
 
-triangulate(1,1,1.41421356237,0,1,1,1,0,1)
+triangulate(1, 1, 1.41421356237, 0, 1, 1, 1, 0, 1)
